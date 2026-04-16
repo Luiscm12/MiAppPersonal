16aa0dc9906896221a148103b980c1ccadae8dba
Luiscm12 <luisangeldelac01@gmail.com>
2026-04-16 10:25:27 -0600

    first commit

---
+++ src/app/tab2/tab2.page.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonAccordion, IonAccordionGroup, IonItem, IonLabel} from '@ionic/angular/standalone';
+import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+
+@Component({
+  selector: 'app-tab2',
+  templateUrl: 'tab2.page.html',
+  styleUrls: ['tab2.page.scss'],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent, IonAccordion, IonAccordionGroup, IonItem, IonLabel]
+})
+export class Tab2Page {
+
+  constructor() {}
+
+}
